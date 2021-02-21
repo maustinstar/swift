626e9603e759d06ad621c590c51567ace33e7305
Michael Verges <maustinstar@gmail.com>
2021-02-20 22:04:43 -0500

    [WIP] Remove VDUC AST walker and refine VDU SIL Pass
    Removes VDUC from misc diagnostics.
    Implements extensinve support for captures and binding switches.
    Adds test cases

--- lib/Sema/TypeCheckStmt.cpp
+++ lib/Sema/TypeCheckStmt.cpp
@@ -2092,9 +2092,6 @@ bool TypeChecker::typeCheckClosureBody(ClosureExpr *closure) {
   }
   closure->setBodyState(ClosureExpr::BodyState::SeparatelyTypeChecked);
   
-  if (!HadError)
-    performClosureBodyDiagnostics(closure);
-  
   return HadError;
 }
 
@@ -2118,7 +2115,6 @@ void TypeChecker::typeCheckTopLevelCodeDecl(TopLevelCodeDecl *TLCD) {
   TLCD->setBody(Body);
   checkTopLevelActorIsolation(TLCD);
   checkTopLevelEffects(TLCD);
-  performTopLevelDeclDiagnostics(TLCD);
 }
 
 void swift::checkUnknownAttrRestrictions(
